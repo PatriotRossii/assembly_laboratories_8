43e22a696ba8dae9bd9cd1821ff3d2ee08c48b17
PatriotRossii <patriotrossii2019@mail.ru>
2023-04-25 18:34:19 +0300

    Add comments in columns_sum.asm

--- src/columns_sum.asm
+++ src/columns_sum.asm
@@ -1,29 +1,40 @@
 global ColumnsSum_
 
 ColumnsSum_:
-        test    edx, edx
-        je      .LBB1_6
+        ; rdi = array, rsi = output
+        ; rdx = rows, rcx = columns
+
         mov     eax, ecx
+
+        ; Смещение, необходимое для перехода к следующей строке
         movsxd  r8, ecx
-        mov     edx, edx
         shl     r8, 2
+
+        ; Инициализация счетчика строк
         xor     r9d, r9d
-        jmp     .LBB1_2
-.LBB1_5:
+        jmp     inner_loop
+outer_loop:
+        ; Увеличение счетчика текущей строки
         inc     r9
+        ; Переход к следующей строке
         add     rdi, r8
+        ; Конец программы, если мы перебрали все строки
         cmp     r9, rdx
-        je      .LBB1_6
-.LBB1_2:
-        test    ecx, ecx
-        je      .LBB1_5
+        je      end
+inner_loop:
+        ; Инициализация счетчика столбцов
         xor     r10d, r10d
-.LBB1_4:
+body:
+        ; Получение значения текущего элемента массива
         mov     r11d, [rdi + 4*r10]
+        ; Обновление элемента выходного массива
         add     [rsi + 4*r10], r11d
+        ; Инкремент счетчика столбцов
         inc     r10
+        ; Если мы еще не перебрали все строки, то продолжаем
         cmp     rax, r10
-        jne     .LBB1_4
-        jmp     .LBB1_5
-.LBB1_6:
+        jne     body
+        ; Иначе выходим во внешний цикл
+        jmp     outer_loop
+end:
         ret
